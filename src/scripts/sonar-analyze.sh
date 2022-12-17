c53743e75e01d697e2c56bc00d835f29c807797f
Ramesh Doddi <rdoddi.pro@gmail.com>
2022-12-17 08:41:01 -0800

    Tests the scripts to run mvn.

--- src/scripts/sonar-analyze.sh
+++ src/scripts/sonar-analyze.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo mvn "${MVN_OPTS:?}" verify sonar:sonar -Dsonar.projectKey="${PROJECT_KEY:?}" -Dsonar.login="${TOKEN:?}"
-mvn "${MVN_OPTS:?}" verify sonar:sonar -Dsonar.projectKey="${PROJECT_KEY:?}" -Dsonar.login="${TOKEN:?}"
+mvn "${MVN_OPTS:?}" clean install
