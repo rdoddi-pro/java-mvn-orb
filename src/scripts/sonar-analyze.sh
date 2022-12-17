ac803844e2342f6919e39aa4f85949eb2d58e401
Ramesh Doddi <rdoddi.pro@gmail.com>
2022-12-17 10:07:44 -0800

    Removes requirement on MVN_OPTS

--- src/scripts/sonar-analyze.sh
+++ src/scripts/sonar-analyze.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mvn --version
-echo mvn "${MVN_OPTS:?}" verify sonar:sonar -Dsonar.projectKey="${PROJECT_KEY:?}" -Dsonar.login="${TOKEN:?}"
-mvn "${MVN_OPTS:?}" clean install
+echo mvn "${MVN_OPTS}" verify sonar:sonar -Dsonar.projectKey="${PROJECT_KEY:?}" -Dsonar.login="${TOKEN:?}"
+mvn "${MVN_OPTS}" clean install
